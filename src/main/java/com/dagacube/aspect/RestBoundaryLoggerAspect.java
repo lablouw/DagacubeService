df0db03de9325dd7e85298f5365e37af83e54ce2
Lloyd <lloyd.louw@mmiholdings.co.za>
2021-12-02 23:46:11 +0200

    Removed bad code

--- src/main/java/com/dagacube/aspect/RestBoundaryLoggerAspect.java
+++ src/main/java/com/dagacube/aspect/RestBoundaryLoggerAspect.java
@@ -76,9 +76,7 @@ public class RestBoundaryLoggerAspect {
             }
         }
 
-        String argsString = sanitizeSecureFieldValues(StringUtils.arrayToCommaDelimitedString(pjp.getArgs()));
-        log.info("Inbound: [method={}, uri={}, args={}, headers={}]", method, uri, argsString, String.join(",", headers));
-
+        log.info("Inbound: [method={}, uri={}, args={}, headers={}]", method, uri, StringUtils.arrayToCommaDelimitedString(pjp.getArgs()), String.join(",", headers));
 
         long t = System.currentTimeMillis();
         Object ret = null;
@@ -102,40 +100,6 @@ public class RestBoundaryLoggerAspect {
         }
 
         return ret;
-    }
-
-    //There must be a better way of doing this, instead of depending on Lombok's toString format
-    private String sanitizeSecureFieldValues(String string) {
-        for (String fieldName : secureFieldNames) {
-
-            while (true) {
-                int indexOfFieldName = string.indexOf(fieldName);
-                if (indexOfFieldName == -1) {
-                    break;
-                }
-                String rightSide = string.substring(indexOfFieldName);
-                int i;
-                int i1 = rightSide.indexOf(", ");
-                int i2 = rightSide.indexOf(")");
-                if (i1 == -1) {
-                    i = i2;
-                } else if (i2 == -1) {
-                    i = i1;
-                } else {
-                    i = Math.min(i1, i2);
-                }
-
-                int lengthOfFieldValue = i - fieldName.length() - 1;
-
-                String newString = string.replace(string.substring(indexOfFieldName + fieldName.length() + 1, indexOfFieldName + fieldName.length() + 1 + lengthOfFieldValue), MASK);
-                if (newString.equals(string)) {
-                    break;
-                }
-                string = newString;
-            }
-
-        }
-        return string;
     }
 
     private boolean isSensitive(String value) {
